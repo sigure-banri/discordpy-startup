2f888f7bcc738215f8ab0e315a284ccc81fe2d97
sigure-banri <82115362+sigure-banri@users.noreply.github.com>
2021-04-08 03:48:17 +0900

    try4

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,10 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    
+@bot.command()
+async def pong(ctx):
+    await ctx.send('ping')
 
 @bot.command()
 async def dice(ctx):
