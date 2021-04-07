b3cfd1061260c422331d5038fc6fa270dd9fc7fb
sigure-banri <82115362+sigure-banri@users.noreply.github.com>
2021-04-08 03:40:51 +0900

    try1

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,39 @@
-from discord.ext import commands
-import os
-import traceback
-
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+import discord
+import sys
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+from func import  diceroll
 
+TOKEN = '任意のトークン'
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+client = discord.Client()
 
+@client.event
+async def on_ready():
+    print('--------------')
+    print('ログインしました')
+    print(client.user.name)
+    print(client.user.id)
+    print('--------------')
+    channel = client.get_channel('チャンネルID')
+    await channel.send('楽しいTRPGを始めましょう！')
 
-bot.run(token)
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    if message.content.startswith("!dice"):
+      
+        say = message.content 
+
+       
+        order = say.strip('!dice ')
+        cnt, mx = list(map(int, order.split('d'))) 
+        dice = diceroll(cnt, mx) 
+        await message.channel.send(dice[cnt])
+        del dice[cnt]
+
+      
+        await message.channel.send(dice)
+
+client.run(TOKEN)
