d8339a2737a393b0c7fe4e162f17a6367e487ece
sigure-banri <82115362+sigure-banri@users.noreply.github.com>
2021-04-08 14:51:57 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,8 +3,7 @@ import os
 import traceback
 import random
 
-# さいころの和を計算する用の関数
-from func import  diceroll
+
 
 
 bot = commands.Bot(command_prefix='/')
@@ -17,35 +16,6 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-@client.event
-async def on_message(message):
-    if message.author.bot:
-        return
-    if message.content.startswith("!dice"):
-        # 入力された内容を受け取る
-        say = message.content 
-
-        # [!dice ]部分を消し、AdBのdで区切ってリスト化する
-        order = say.strip('!dice ')
-        cnt, mx = list(map(int, order.split('d'))) # さいころの個数と面数
-        dice = diceroll(cnt, mx) # 和を計算する関数(後述)
-        await message.channel.send(dice[cnt])
-        del dice[cnt]
-
-        # さいころの目の総和の内訳を表示する
-        await message.channel.send(dice)
-        
-def diceroll(cnt, max):
-    total = 0
-    num_list = []
-    for i in range(0, cnt):
-        # ランダムに1からサイコロの面数までの和を取得しリストに入れる
-        num = random.randint(1, max)
-        num_list.append(num)
-    # さいころの目の総和を計算しリストに入れる
-    total = sum(num_list)
-    num_list.append(total)
-    return num_list
 
     
 guraindo1 = '１', '中吉だ', '吉だ。', '半吉だ。', '半中吉だ。', '半大吉だ。', '凶だ。', '中凶だ。', '大凶だ。', '半凶だ。', '半中凶だ。', '半大凶だ。', '不明だ……。', '超絶大吉だ！'
