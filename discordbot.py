198f268eb99e5211060f8e5a53f635b5f7ac2daf
sigure-banri <82115362+sigure-banri@users.noreply.github.com>
2021-04-08 18:04:18 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -26,6 +26,13 @@ async def kyouun(ctx):
     # await ctx.send("{}へ：今日の運勢だ。".format(ctx.message.author.name))
     await ctx.send(f"{ctx.message.author.name}へ：今日の運勢だ。\n{ran1}")
     
+guraindo2 =  '半凶', '半中凶', '半大凶', '不明', '超絶大吉'
+@bot.command(name="運勢a", pass_context=True)
+async def kyouun(ctx):
+    ran1 = random.choice(guraindo1)
+    # await ctx.send("{}へ：今日の運勢だa。".format(ctx.message.author.name))
+    await ctx.send(f"{ctx.message.author.name}へ：今日の運勢だa。\n{ran1}")
+    
 
 @bot.command()
 async def ping(ctx):
