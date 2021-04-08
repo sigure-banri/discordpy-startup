41124adce8f0ca5a9fdf40e0a2b98ceb2084772a
sigure-banri <82115362+sigure-banri@users.noreply.github.com>
2021-04-08 15:06:39 +0900

    ためし

--- discordbot.py
+++ discordbot.py
@@ -17,6 +17,18 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+    
+    
+@client.event
+async def on_message(message):
+    # 「おはよう」で始まるか調べる
+    if message.content.startswith("おはよう"):
+        # 送り主がBotだった場合反応したくないので
+        if client.user != message.author:
+            # メッセージを書きます
+            m = "おはようございます" + message.author.name + "さん！"
+            # メッセージが送られてきたチャンネルへメッセージを送ります
+            await message.channel.send(m)
 
 
 @bot.event
