92c15fc397a6afc91da7df0b236ce09f080303cc
sigure-banri <82115362+sigure-banri@users.noreply.github.com>
2021-04-08 14:38:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,12 +14,20 @@ async def on_command_error(ctx, error):
     await ctx.send(error_msg)
 
 
-guraindo1 = '大吉だ。', '中吉だ', '吉だ。', '半吉だ。', '半中吉だ。', '半大吉だ。', '凶だ。', '中凶だ。', '大凶だ。', '半凶だ。', '半中凶だ。', '半大凶だ。', '不明だ……。', '超絶大吉だ！'
+guraindo1 = '１０００万円', '５０００万円', '１億円', '２億円', '３億円'
+@bot.command(name="ポイント内訳", pass_context=True)
+async def kyouunn(ctx):
+    ran1 = random.choice(guraindo1)
+    # await ctx.send("{}へ：今日のポイントだ。".format(ctx.message.author.name))
+    await ctx.send(f"{ctx.message.author.name}へ：今日のポイントだ。\n{ran1}")
+    
+guraindo1 = '１', '中吉だ', '吉だ。', '半吉だ。', '半中吉だ。', '半大吉だ。', '凶だ。', '中凶だ。', '大凶だ。', '半凶だ。', '半中凶だ。', '半大凶だ。', '不明だ……。', '超絶大吉だ！'
 @bot.command(name="運勢", pass_context=True)
 async def kyouun(ctx):
     ran1 = random.choice(guraindo1)
     # await ctx.send("{}へ：今日の運勢だ。".format(ctx.message.author.name))
     await ctx.send(f"{ctx.message.author.name}へ：今日の運勢だ。\n{ran1}")
+    
 
 @bot.command()
 async def ping(ctx):
