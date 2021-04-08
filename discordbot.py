2d12f4a580c6cfdad0e91a905aadafc0685c3ecb
sigure-banri <82115362+sigure-banri@users.noreply.github.com>
2021-04-08 14:46:58 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,6 +3,10 @@ import os
 import traceback
 import random
 
+# さいころの和を計算する用の関数
+from func import  diceroll
+
+
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
@@ -13,13 +17,36 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    if message.content.startswith("!dice"):
+        # 入力された内容を受け取る
+        say = message.content 
 
-guraindo1 = '１０００万円', '５０００万円', '１億円', '２億円', '３億円'
-@bot.command(name="ポイント内訳", pass_context=True)
-async def kyouunn(ctx):
-    ran1 = random.choice(guraindo1)
-    # await ctx.send("{}へ：今日のポイントだ。".format(ctx.message.author.name))
-    await ctx.send(f"{ctx.message.author.name}へ：今日のポイントだ。\n{ran1}")
+        # [!dice ]部分を消し、AdBのdで区切ってリスト化する
+        order = say.strip('!dice ')
+        cnt, mx = list(map(int, order.split('d'))) # さいころの個数と面数
+        dice = diceroll(cnt, mx) # 和を計算する関数(後述)
+        await message.channel.send(dice[cnt])
+        del dice[cnt]
+
+        # さいころの目の総和の内訳を表示する
+        await message.channel.send(dice)
+        
+def diceroll(cnt, max):
+    total = 0
+    num_list = []
+    for i in range(0, cnt):
+        # ランダムに1からサイコロの面数までの和を取得しリストに入れる
+        num = random.randint(1, max)
+        num_list.append(num)
+    # さいころの目の総和を計算しリストに入れる
+    total = sum(num_list)
+    num_list.append(total)
+    return num_list
+
     
 guraindo1 = '１', '中吉だ', '吉だ。', '半吉だ。', '半中吉だ。', '半大吉だ。', '凶だ。', '中凶だ。', '大凶だ。', '半凶だ。', '半中凶だ。', '半大凶だ。', '不明だ……。', '超絶大吉だ！'
 @bot.command(name="運勢", pass_context=True)
