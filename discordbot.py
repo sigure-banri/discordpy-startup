049b2743c19bc6c868e724050113617cf28a60ae
sigure-banri <82115362+sigure-banri@users.noreply.github.com>
2021-04-08 14:11:39 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -13,10 +13,7 @@ async def on_command_error(ctx, error):
     await ctx.send(error_msg)
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-    
+
     guraindo1 = '大吉だ。', '中吉だ', '吉だ。', '半吉だ。', '半中吉だ。', '半大吉だ。', '凶だ。', '中凶だ。', '大凶だ。', '半凶だ。', '半中凶だ。', '半大凶だ。', '不明だ……。', '超絶大吉だ！'
 @bot.command(name="運勢", pass_context=True)
 async def kyouun(ctx):
@@ -24,6 +21,10 @@ async def kyouun(ctx):
     # await ctx.send("{}へ：今日の運勢だ。".format(ctx.message.author.name))
     await ctx.send(f"{ctx.message.author.name}へ：今日の運勢だ。\n{ran1}")
 
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+        
 @bot.command()
 async def pong(ctx):
     await ctx.send('ping')
